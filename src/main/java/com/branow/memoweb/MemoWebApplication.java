cfd4d7cd4c46c25ff19ac10d158688c592a9623c
branow <bodnarorestdraw@gmail.com>
2023-10-17 11:24:11 +0300

    init commit

---
+++ src/main/java/com/branow/memoweb/MemoWebApplication.java
@@ -0,0 +1,13 @@
+package com.branow.memoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/branow/memoweb/MemoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.branow.memoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
