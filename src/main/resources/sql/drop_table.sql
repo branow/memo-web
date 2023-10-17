944e4fca26944c005131b1389a0719cc2cd4bd3a
branow <bodnarorestdraw@gmail.com>
2023-10-17 12:51:04 +0300

    add drop_table.sql script

---
+++ src/main/resources/sql/drop_table.sql
@@ -0,0 +1,12 @@
+drop table if exists score;
+drop table if exists study_type;
+drop table if exists flashcard;
+drop table if exists formatted_text;
+drop table if exists media;
+drop table if exists collection;
+drop table if exists module;
+drop table if exists access_type;
+drop table if exists verification_token;
+drop table if exists user;
+
+
