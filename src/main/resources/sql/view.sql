e10817cd8bbb80789026f2a4a30a8fed39eb4633
branow <bodnarorestdraw@gmail.com>
2023-12-14 16:26:55 +0200

    fix view and fix calc reset time

--- src/main/resources/sql/view.sql
+++ src/main/resources/sql/view.sql
@@ -9,4 +9,4 @@ select f.flashcard_id,
     left join formatted_text ftf on f.front_side = ftf.text_id
     left join formatted_text ftb on f.back_side = ftb.text_id
     left join collection c on f.collection = c.collection_id
-    left join module m on c.module = m.module_id
+    left join module m on c.module = m.module_id;
